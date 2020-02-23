1d5f90a1c7793bf596d37f1f35618cb68c212aba
bebaek <bebaek@gmail.com>
2020-02-23 10:47:14 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+from photodetector import __version__
+
+setup(
+    name='photodetector',
+    version=__version__,
+    python_requires='>3.6',
+    packages=find_packages(),
+    package_data={
+        '': ['data/*.jpg'],
+    },
+    install_requires=[
+        'matplotlib',
+        'opencv-contrib-python',
+    ],
+    entry_points={
+        'console_scripts': [
+            'photodetector = photodetector.__main__:main',
+        ],
+    },
+)
